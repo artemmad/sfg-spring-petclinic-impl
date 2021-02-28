b54ee22a9c4ccce29b71aef07e945fd4d2c6c277
artemmakarevich1997 <artemmakarevich1997@gmail.com>
2021-02-28 23:46:45 +0300

    Start

---
+++ src/test/java/ru/realityfamily/sfgspringpetclinic/SfgSpringPetclinicApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.realityfamily.sfgspringpetclinic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SfgSpringPetclinicApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
