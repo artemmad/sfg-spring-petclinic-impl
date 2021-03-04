4c547f63a603bc77f36140939c7715da19a885c0
artemmakarevich1997 <artemmakarevich1997@gmail.com>
2021-03-04 19:17:07 +0300

    Adding POJO's. Closes #8.

---
+++ src/main/java/ru/realityfamily/sfgspringpetclinic/model/Owner.java
@@ -0,0 +1,4 @@
+package ru.realityfamily.sfgspringpetclinic.model;
+
+public class Owner extends Person{
+}
---
+++ src/main/java/ru/realityfamily/sfgspringpetclinic/model/Vet.java
@@ -0,0 +1,4 @@
+package ru.realityfamily.sfgspringpetclinic.model;
+
+public class Vet extends Person{
+}
