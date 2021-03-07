bdce7ea270de38390ddf56436d279108bb3e785c
artemmakarevich1997 <artemmakarevich1997@gmail.com>
2021-03-08 00:24:42 +0300

    Adding base entity. Closes #10

--- pet-clinic-data/src/main/java/ru/realityfamily/sfgspringpetclinic/model/Person.java
+++ pet-clinic-data/src/main/java/ru/realityfamily/sfgspringpetclinic/model/Person.java
@@ -1,6 +1,6 @@
 package ru.realityfamily.sfgspringpetclinic.model;
 
-public class Person {
+public class Person extends  BaseEntity{
     private String firstName;
     private String lastName;
 
--- pet-clinic-data/src/main/java/ru/realityfamily/sfgspringpetclinic/model/PetType.java
+++ pet-clinic-data/src/main/java/ru/realityfamily/sfgspringpetclinic/model/PetType.java
@@ -1,6 +1,6 @@
 package ru.realityfamily.sfgspringpetclinic.model;
 
-public class PetType {
+public class PetType extends BaseEntity {
     private String name;
 
     public String getName() {
